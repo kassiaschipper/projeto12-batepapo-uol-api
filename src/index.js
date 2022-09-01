3e9af5183c3898ae0b5708fe7f41b2a895fdec8e
Kássia <kassia_guterres@outlook.com>
2022-09-01 14:59:12 -0300

    get/post participants - work in progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import Joi from "joi";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -15,6 +16,40 @@ let db;
 mongoClient.connect().then(() => {
   db = mongoClient.db("bate-papo-uol");
 });
+
+
+server.get("/participants", (request, response) => {
+    db.collection("participants").find().toArray().then((data) => {
+        response.send(data.map(value => ({ //não pegar o _id
+            ...value,
+            _id: undefined,
+        })));
+    })
+})
+
+server.post("/participants", (request, response) => {
+
+     const { name } = request.body;
+
+     //mudar para validação com Joi
+    if(!name || typeof(name) !== "string" || name.length === 0){
+       return response.sendStatus(422);
+    }
+   
+    
+    //Verificar se o nome já foi usado
+    //const repeatedName = db.collection("participants").findOne(name); 
+
+     db.collection("participants").insertOne({ name }).then(() =>{
+        // console.log(name);
+        // if(repeatedName){
+        //     console.log("nome repetido")
+        //     response.sendStatus(409);
+        // }
+        response.sendStatus(201);
+    });
+
+})
 
 
 server.listen(5000, function () {
