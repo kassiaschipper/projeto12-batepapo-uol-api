bb9553bc242b5feeb636716712657ef0d9684b98
Kássia <kassia_guterres@outlook.com>
2022-09-01 12:58:38 -0300

    connecting to mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb"; 
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,9 +8,15 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-console.log(process.env.TESTE, process.env.MONGO_URI) 
-const mongoClient = new MongoClient(process.env.MONGO_URI) 
+//console.log(process.env.TESTE, process.env.MONGO_URI)
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("bate-papo-uol");
+});
+
 
 server.listen(5000, function () {
-    console.log("Listening on Port 5000");
-})
+  console.log("Listening on Port 5000");
+});
