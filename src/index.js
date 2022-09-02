1f5c4d996b0b676ed3a63780c88ad03ba4206799
Kássia <kassia_guterres@outlook.com>
2022-09-02 15:05:50 -0300

    inserting input message into mongo db

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import joi from "joi";
+import dayjs from "dayjs";
 import dotenv from "dotenv";
 dotenv.config();
 
 const userSchema = joi.object({
-  name: joi.string().required(),
+  name: joi.string().required().min(1),
 });
 
 const server = express();
@@ -20,7 +21,8 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("bate-papo-uol");
 });
 
-// #Rota participants 
+
+// #Rota participants
 server.get("/participants", async (request, response) => {
   try {
     const participantsResponse = await db
@@ -46,26 +48,35 @@ server.post("/participants", async (request, response) => {
   const filterName = await db
     .collection("participants")
     .find({ name })
-    .toArray(); 
+    .toArray();
 
   //valida se o nome é uma string e se não é vazio
   if (validation.error) {
-    response.send("Preencha o campo corretamente").status(422);
+    response.status(422).send("Preencha o campo corretamente");
     return;
   }
   //Verifica se o nome passado já existe no db
-  //Se o tamanho da array for mair que 0 significa que o nome já existe 
-  if (filterName.length > 0) {  
-    response.send("Usuário já existente").status(409);
+  //Se o tamanho da array for mair que 0 significa que o nome já existe
+  if (filterName.length > 0) {
+    response.status(409).send("Usuário já existente");
     return;
   }
 
   try {
     const participantsResponse = await db
       .collection("participants")
-      .insertOne({ name });
+      .insertOne({ name, lastStatus: Date.now() });
+    const message = await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().locale("pt-br").format("HH:mm:ss"),
+    });
     response.sendStatus(201);
+    
   } catch (error) {
+    console.log(error)
     response.sendStatus(500);
   }
 });
