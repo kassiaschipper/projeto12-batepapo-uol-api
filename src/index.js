4a377e01e4daee18dea5a45f75ab2715e196e4e3
Kássia <kassia_guterres@outlook.com>
2022-09-02 10:59:56 -0300

    get/post participants with async/await and joi

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import Joi from "joi";
+import joi from "joi";
 import dotenv from "dotenv";
 dotenv.config();
+
+const userSchema = joi.object({
+  name: joi.string().required(),
+});
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-//console.log(process.env.TESTE, process.env.MONGO_URI)
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 let db;
@@ -17,40 +20,46 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("bate-papo-uol");
 });
 
+server.get("/participants", async (request, response) => {
+  try {
+    const participantsResponse = await db
+      .collection("participants")
+      .find()
+      .toArray();
+    response.send(
+      participantsResponse.map((value) => ({
+        ...value,
+        _id: undefined,
+      }))
+    );
+  } catch (error) {
+    console.log(error);
+    response.sendStatus(500);
+  }
+});
 
-server.get("/participants", (request, response) => {
-    db.collection("participants").find().toArray().then((data) => {
-        response.send(data.map(value => ({ //não pegar o _id
-            ...value,
-            _id: undefined,
-        })));
-    })
-})
+server.post("/participants", async (request, response) => {
+  const { name } = request.body;
 
-server.post("/participants", (request, response) => {
+  const validation = userSchema.validate({ name }, { abortEarly: false });
 
-     const { name } = request.body;
+  if (validation.error) {
+    response.sendStatus(422);
+    return;
+  }
 
-     //mudar para validação com Joi
-    if(!name || typeof(name) !== "string" || name.length === 0){
-       return response.sendStatus(422);
-    }
-   
-    
-    //Verificar se o nome já foi usado
-    //const repeatedName = db.collection("participants").findOne(name); 
-
-     db.collection("participants").insertOne({ name }).then(() =>{
-        // console.log(name);
-        // if(repeatedName){
-        //     console.log("nome repetido")
-        //     response.sendStatus(409);
-        // }
-        response.sendStatus(201);
-    });
-
-})
-
+  //Verificar se o nome já foi usado
+  //const repeatedName = db.collection("participants").findOne(name);
+  try {
+    const participantsResponse = await db
+      .collection("participants")
+      .insertOne({ name });
+    response.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    response.sendStatus(500);
+  }
+});
 
 server.listen(5000, function () {
   console.log("Listening on Port 5000");
